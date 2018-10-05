e8f25073b1b40666c5b3d892594348f38cdf6b10
lyuan11_cs <lyuan9237@gmail.com>
2018-10-05 01:09:31 -0500

    Template

---
+++ Binary Search/Binary-Search-Template.java
@@ -0,0 +1,30 @@
+// The array must be sorted
+public class Solution{
+	public int findPosition(int[] nums, int target){
+		if(nums == null || nums.length == 0){
+			return -1;
+		}
+
+		int start = 0, end = nums.length - 1;
+
+		while(start + 1 < end || start <= end) { //loop ends condition 
+			int mid = start + (end - start) / 2;
+			if(nums[mid] == target){
+				return mid;
+			}else if (nums[mid] < target){
+				start = mid;
+			}else{
+				end = mid;
+			}
+		}
+
+		if(nums[start] == target）{
+			return start;
+		}
+		if(nums[end] == target){
+			return end;
+		}
+
+		return -1;
+	}
+}
