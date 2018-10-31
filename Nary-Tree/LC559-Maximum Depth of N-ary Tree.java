8f81ead52db541e06a28cfabc90e9ce75bab1304
lyuan11_cs <lyuan9237@gmail.com>
2018-10-31 00:34:47 -0500

    N-ary Tree

---
+++ Nary-Tree/LC559-Maximum Depth of N-ary Tree.java
@@ -0,0 +1,76 @@
+Recursion: 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+   
+    private int max = 0;
+    public int maxDepth(Node root) {
+        if (root == null) return 0;
+        maxDepth(root, 1);
+        return max;
+    } 
+    
+    public void maxDepth(Node node, int depth) {
+        if (node == null) return;
+        max = Math.max(max, depth);
+        for (Node child : node.children){
+            maxDepth(child, depth+1);
+        }
+    }
+}
+
+
+
+Iteration: 
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val,List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+    public int maxDepth(Node root) {
+    if(root == null) 
+        return 0;
+    
+    Queue<Node> queue = new LinkedList<>();
+    queue.offer(root);
+    
+    int depth = 0;
+    
+    while(!queue.isEmpty())
+    {
+        int size = queue.size();
+        
+        for(int i = 0; i < size; i++)
+        {
+            Node current = queue.poll();
+            for(Node child: current.children) queue.offer(child);
+        }
+        
+        depth++;
+    }
+    
+    return depth;
+    }
+}
