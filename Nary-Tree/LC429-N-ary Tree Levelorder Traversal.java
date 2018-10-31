9005366b4db9f738e348b2a4e7231b85508373e0
lyuan11-cs <43146809+lyuan11-cs@users.noreply.github.com>
2018-10-31 00:40:25 -0500

    Update LC429-N-ary Tree Levelorder Traversal.java

--- Nary-Tree/LC429-N-ary Tree Levelorder Traversal.java
+++ Nary-Tree/LC429-N-ary Tree Levelorder Traversal.java
@@ -1,4 +1,28 @@
 /*
+Given an n-ary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level).
+
+For example, given a 3-ary tree:
+             1
+           / | \
+          3  2  4
+         / \
+        5   6 
+We should return its level order traversal:
+
+[
+     [1],
+     [3,2,4],
+     [5,6]
+]
+ 
+
+Note:
+
+The depth of the tree is at most 1000.
+The total number of nodes is at most 5000.
+*/
+
+/*
 // Definition for a Node.
 class Node {
     public int val;
