4ee5e388b71f9491b565e8972fb37ebf37fce296
lyuan11-cs <43146809+lyuan11-cs@users.noreply.github.com>
2018-10-31 00:47:26 -0500

    Update LC590-N-ary Tree Postorder Traversal.java

--- Nary-Tree/LC590-N-ary Tree Postorder Traversal.java
+++ Nary-Tree/LC590-N-ary Tree Postorder Traversal.java
@@ -1,3 +1,16 @@
+/*
+Given an n-ary tree, return the postorder traversal of its nodes' values.
+
+For example, given a 3-ary tree:
+             1
+           / | \
+          3  2  4
+         / \
+        5   6 
+Return its postorder traversal as: [5,6,3,2,4,1].
+
+*/
+
 Recursion: 
 /*
 // Definition for a Node.
